9d76251dcb02e1be4aeedd98512a2b429b4e4d2f
Lasertie Zyglonk <zyglonk@proton.me>
2025-03-11 09:11:30 +0100

    chang port

--- setup.py
+++ setup.py
@@ -31,4 +31,4 @@ def predict():
     return jsonify({"predicted_usage": predicted_usage[0]})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=7777)
