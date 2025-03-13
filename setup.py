62fcae408168f3ef8473b31e2adec64078ba4b41
Lasertie Zyglonk <zyglonk@proton.me>
2025-03-13 08:27:01 +0100

    go

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+import tensorflow as tf
 
 app = Flask(__name__)
 
