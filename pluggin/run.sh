d8755c13f520c444eb9789880ddfb159f739a6ed
Lasertie Zyglonk <zyglonk@proton.me>
2025-05-12 16:07:13 +0200

    Nettoyage

---
+++ pluggin/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bashio
+
+echo "Démarrage du service d'IA prédictive pour le chauffage..."
+
+# installer les dépendances
+pip3 -r requires.txt
+
+# Appeler le script Python
+python3 /setup.py       
