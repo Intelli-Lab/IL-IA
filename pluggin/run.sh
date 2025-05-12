f73ea9f95edac66aa7c5fdba1253776faafba3c4
Lasertie Zyglonk <zyglonk@proton.me>
2025-05-12 16:09:15 +0200

    Revert "Nettoyage"
    This reverts commit d8755c13f520c444eb9789880ddfb159f739a6ed.

--- pluggin/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-echo "Démarrage du service d'IA prédictive pour le chauffage..."
-
-# installer les dépendances
-pip3 -r requires.txt
-
-# Appeler le script Python
-python3 /setup.py       
